ee97cb84143a6e67f797671db3b83839dd582cc9
yuan.chen <yuan.chen@electrifai.com>
2024-06-17 17:22:55 +0800

    feat

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -1,20 +1,94 @@
-import { TaobaoSquareFilled } from "@ant-design/icons";
-import { Input } from "antd";
-import './header.sass'
+import {
+  TaobaoSquareFilled,
+  UserOutlined,
+  MenuOutlined,
+  CaretRightFilled,
+  DownOutlined
+} from "@ant-design/icons";
+import { Input, Dropdown, theme, Image } from "antd";
+import type { DropdownProps } from "antd";
+import "./header.sass";
+import { useState } from "react";
 const { Search } = Input;
+const { useToken } = theme;
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpenChange: DropdownProps["onOpenChange"] = (nextOpen, info) => {
+    if (info.source === "trigger" || nextOpen) {
+      setOpen(nextOpen);
+    }
+  };
   return (
     <>
-      <div className="ml-2 p-4 bg-white flex items-center header">
-        <TaobaoSquareFilled style={{ fontSize: "60px", color: "red" }} />
-        <span className="ml-4 font-bold text-2xl text-red-500">Shopping</span>
-        <Search placeholder="Search for products here" size='large' style={{ width: '50%',background:'#eee' }} className="ml-4 bg-[#eee]"/>
-        <div className="ml-auto">
-            <span>注册</span>
-            <span>登录</span>
+      <div className="bg-white">
+        <div className="ml-2 p-4  flex items-center header">
+          <TaobaoSquareFilled style={{ fontSize: "60px", color: "red" }} />
+          <span className="ml-4 font-bold text-2xl text-red-500">Shopping</span>
+          <Search
+            placeholder="Search for products here"
+            size="large"
+            style={{ width: "50%", background: "#eee" }}
+            className="ml-4 bg-[#eee]"
+          />
+          <div className="ml-auto ">
+            <span className="mr-4 cursor-pointer">register</span>
+            <span className="mr-4 text-blue-400 cursor-pointer">login</span>
+            <UserOutlined style={{ fontSize: "20px" }} />
+          </div>
+        </div>
+        <div className="ml-2 p-4 inline-flex items-center cursor-pointer">
+          <Dropdown
+            overlayStyle={{ width: "100%" }}
+            dropdownRender={() => <NodeC></NodeC>}
+            open={open}
+            onOpenChange={handleOpenChange}
+          >
+            <div>
+              <MenuOutlined style={{ fontSize: "20px" }} />
+              <span className="ml-2">commodity Classification</span>
+              <DownOutlined style={{marginLeft:'1rem',color:'red'}}/>
+            </div>
+          </Dropdown>
         </div>
       </div>
     </>
   );
 }
+
+const NodeC = () => {
+  const { token } = useToken();
+  const contentStyle: React.CSSProperties = {
+    backgroundColor: token.colorBgElevated,
+    borderRadius: token.borderRadiusLG,
+    boxShadow: token.boxShadowSecondary,
+  };
+  return (
+    <div className="p-8 bg-white flex" style={contentStyle}>
+      <div className="">
+        <div className="flex items-center">
+          <Image
+            width={"2rem"}
+            style={{ borderRadius: "50%" }}
+            src="http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Ficons%2FPkqdPDirMxW6m5J32pQSy.webp&w=640&q=100"
+          />
+          <span className="ml-2 text-xl">Fashion and Clothing</span>
+        </div>
+        <div className="flex items-center">
+        <Image
+            width={"2rem"}
+            style={{ borderRadius: "50%" }}
+            src="http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Ficons%2F5WqXrtYDMWbZGP70y2wAR.webp&w=640&q=100"
+          />
+          <span className="ml-2 text-xl">Digital Products</span>
+        </div>
+      </div>
+      <div className="ml-8 flex flex-col justify-between">
+        <div className="ml-2 text-xl pl-2" style={{borderLeft:'5px solid red'}}>shoe<CaretRightFilled style={{color:'red'}}/></div>
+        <div className="ml-2 text-xl">Women's Shoes</div>
+        <div className="ml-2 text-xl">Men's Clothing and Shoes</div>
+      </div>
+    </div>
+  );
+};
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,207 @@
+import { Carousel, Image } from "antd";
+import { useMemo } from "react";
 export default function Home() {
-    return (
-      <>
-        <div>home</div>
-      </>
-    );
-  }
-  
+  const imgList = useMemo(() => {
+    return [
+      {
+        sale: "25%",
+        historical_prices: "2887",
+        prices: "3850",
+        url: "http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Fproducts%2FS-0bkuZHpxoB0-eaxVrLe.webp&w=640&q=100",
+      },
+      {
+        sale: "24%",
+        historical_prices: "59.28",
+        prices: "79",
+        url: "http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Fproducts%2FX9AGWqXgzr7bgrc_-vL9q.webp&w=640&q=100",
+      },
+      {
+        sale: "18%",
+        historical_prices: "12300",
+        prices: "15000",
+        url: "http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Fproducts%2Fn0sPwJaCzVYI4yZN_EANK.webp&w=640&q=100",
+      },
+      {
+        sale: "17%",
+        historical_prices: "664",
+        prices: "800",
+        url: "http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Fproducts%2FeIleut5TAwdnYitRE_bKU.webp&w=828&q=100",
+      },
+      {
+        sale: "15%",
+        historical_prices: "92.65",
+        prices: "109",
+        url: "http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Fproducts%2F96PIDc76xhxAB1v0IMtM2.webp&w=828&q=100",
+      },
+      {
+        sale: "15%",
+        historical_prices: "2507.5",
+        prices: "2950",
+        url: "http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Fproducts%2F0FXkap3UldFhXBlGVT4qU.webp&w=750&q=100",
+      },
+      {
+        sale: "15%",
+        historical_prices: "11900",
+        prices: "14000",
+        url: "http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Fproducts%2F9ITjskMj53AKi3X1d72ys.webp&w=640&q=100",
+      },
+      {
+        sale: "13%",
+        historical_prices: "1305",
+        prices: "1500",
+        url: "http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Fproducts%2FsV-PTzdN1MR2KhSX8zUea.webp&w=828&q=100",
+      },
+      {
+        sale: "10%",
+        historical_prices: "116.1",
+        prices: "129",
+        url: "http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Fproducts%2F3a-sGCVFKOm-EdY0NrBoG.webp&w=750&q=100",
+      },
+    ];
+  }, []);
+  return (
+    <div>
+      <div
+        className="cursor-pointer"
+        style={{ borderRadius: "1rem", overflow: "hidden" }}
+      >
+        <Carousel autoplay autoplaySpeed={1500}>
+          <div className="text-center">
+            <Image
+              preview={false}
+              width={"80%"}
+              style={{ borderRadius: "1rem", height: "25rem" }}
+              src="http://huanghanzhilian-test.oss-cn-beijing.aliyuncs.com/shop/upload/image/sliders/hWQ4-Mx69MyLJbZAThWEt.webp"
+            />
+          </div>
+          <div className="text-center">
+            <Image
+              preview={false}
+              width={"80%"}
+              style={{ borderRadius: "1rem", height: "25rem" }}
+              src="http://huanghanzhilian-test.oss-cn-beijing.aliyuncs.com/shop/upload/image/sliders/hUX6oL-lCKOKPYbZ5j4rx.webp"
+            />
+          </div>
+          <div className="text-center">
+            <Image
+              preview={false}
+              width={"80%"}
+              style={{ borderRadius: "1rem", height: "25rem" }}
+              src="http://huanghanzhilian-test.oss-cn-beijing.aliyuncs.com/shop/upload/image/sliders/g8FHsxbCGw82WzjmamElL.webp"
+            />
+          </div>
+        </Carousel>
+        <div className="mt-8 ml-8 flex px-[8rem] justify-center	 mt-[6rem]">
+          {imgList.map((i) => {
+            return (
+              <div
+                key={i.url}
+                style={{
+                  borderLeft: "3px solid red",
+                  borderRight: "3px solid red",
+                }}
+                className="px-4"
+              >
+                <Image
+                  preview={false}
+                  style={{
+                    borderRadius: "1rem",
+                    height: "7rem",
+                    width: "6rem",
+                  }}
+                  src={i.url}
+                />
+                <div className="flex items-center">
+                  <div
+                    className="py-1 px-2 bg-[red] text-white font-bold"
+                    style={{ borderRadius: "1rem" }}
+                  >
+                    {i.sale}
+                  </div>
+                  <span className="ml-4 font-bold text-xl ">{i.prices}</span>
+                </div>
+                <div className="float-right ">
+                  <span className="line-through">{i.historical_prices}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="text-center mt-12 text-xl">
+          <span className="text-red-500">Featured Products</span> -{" "}
+          <span>all categories</span>
+          <div className="mt-8 flex justify-center">
+            <span>
+              <Image
+                preview={false}
+                style={{
+                  borderRadius: "1rem",
+                  height: "10rem",
+                  width: "10rem",
+                  marginRight: "3rem",
+                }}
+                src="http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Ficons%2FPkqdPDirMxW6m5J32pQSy.webp&w=750&q=100"
+              />
+              <div>Fashion and Clothing</div>
+            </span>
+            <span>
+              <Image
+                preview={false}
+                style={{
+                  borderRadius: "1rem",
+                  height: "10rem",
+                  width: "10rem",
+                }}
+                src="http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2F%2Ficons%2F5WqXrtYDMWbZGP70y2wAR.webp&w=828&q=100"
+              />
+              <div>Digital Products</div>
+            </span>
+          </div>
+        </div>
+
+        <div className="w-[100%] flex justify-center mt-12">
+          <Image
+            preview={false}
+            width={"40%"}
+            style={{
+              borderRadius: "1rem",
+              height: "20rem",
+            }}
+            src="http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2Fbanners%2FAAR1hdzMBEfpYKym3njGU.jpeg&w=1080&q=100"
+          />
+          <span className="mx-2"></span>
+          <Image
+            preview={false}
+            width={"40%"}
+            style={{
+              borderRadius: "1rem",
+              height: "20rem",
+            }}
+            src="http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2Fbanners%2FtHzPZwswSaFdD_3TpdPCt.jpeg&w=750&q=100"
+          />
+        </div>
+        <div className="w-[100%] flex justify-center mt-4">
+          <Image
+            preview={false}
+            width={"40%"}
+            style={{
+              borderRadius: "1rem",
+              height: "20rem",
+            }}
+            src="http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2Fbanners%2FluBUyOForM7vLS8SMMORT.jpeg&w=828&q=100"
+          />
+          <span className="mx-2"></span>
+          <Image
+            preview={false}
+            width={"40%"}
+            style={{
+              borderRadius: "1rem",
+              height: "20rem",
+            }}
+            src="http://shop.huanghanlian.com/_next/image?url=http%3A%2F%2Fhuanghanzhilian-test.oss-cn-beijing.aliyuncs.com%2Fshop%2Fupload%2Fimage%2Fbanners%2FAG8T4X-3pFnHc1O2XEeN5.jpeg&w=1080&q=100"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
